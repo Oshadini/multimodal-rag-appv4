2937e6a1e0d631fc726e1e556681b4f814332662
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-16 21:01:12 +0530

    Update MuRAGAppLatency1.py

--- MuRAGAppLatency1.py
+++ MuRAGAppLatency1.py
@@ -572,13 +572,16 @@ if uploaded_file is not None:
         unsafe_allow_html=True,
     )
 
-        # Display custom buttons
     st.markdown(
         """
-    
-            <button class="custom-button">Custom Button 1</button>
-        
-        
+        <div style="display: flex; justify-content: center;">
+            <div style="margin-right: 10px;">
+                <button class="custom-button">Custom Button 1</button>
+            </div>
+            <div>
+                <button class="custom-button">Custom Button 2</button>
+            </div>
+        </div>
         """,
         unsafe_allow_html=True
     )
