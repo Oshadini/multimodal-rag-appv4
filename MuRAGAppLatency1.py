b7611a5f5e86f8458cb52d3c6d0959e4c1f9a3a2
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-16 16:23:48 +0530

    Update MuRAGAppLatency1.py

--- MuRAGAppLatency1.py
+++ MuRAGAppLatency1.py
@@ -54,7 +54,7 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 
 
-st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title="InfoGenie",layout='wide', initial_sidebar_state='expanded')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
